27dd109dec936d66a0510333e102bffcebf1bfed
Brian Whicheloe <brian@Brians-MacBook-Pro-3.local>
2017-03-11 22:26:20 -0800

    Add analytics

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import router from './routes.js'
 import app from './components/app.vue'
+import VueAnalytics from 'vue-analytics'
+
+let id = process.env.NODE_ENV === 'production' ? 'UA-28431964-14' : 'UA-93531536-3'
+Vue.use(VueAnalytics, {id, router})
 
 new Vue({
     router,
