471e0edc86626b4b546b97efc3617bfff5e1e0cb
Brian Whicheloe <brian@whicheloe.us>
2018-12-02 17:30:02 -0800

    Add google analytics

--- client/entry_client.js
+++ client/entry_client.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-// import VueAnalytics from 'vue-analytics'
+import VueAnalytics from 'vue-analytics'
 import createApp from './create_app.js'
+import {gaDevId, gaProductionId} from '../config.js'
 
 const {app, store, router} = createApp()
 
@@ -8,31 +9,11 @@ if (window.__INITIAL_STATE__) {
     store.replaceState(window.__INITIAL_STATE__)
 }
 
-// let id = ENV === 'production' ? 'UA-28431964-14' : 'UA-93531536-3'
-
-// TODO
-// CHANGEME
-// let gaID
-// if (process.env.NODE_ENV === 'development') {
-//     gaID = 'UA-XXX-X' // Dev gaID
-// } else {
-//     gaID = 'UA-XXX-X' // Live gaID
-// }
-
-// Vue.use(VueAnalytics, {
-//     id: gaID,
-//     router,
-//     ignoreRoutes: ['admin'],
-//     ecommerce: {
-//         enabled: true,
-//         enhanced: true,
-//     },
-//     // debug: {
-//     //     enabled: true,
-//     //     trace: false,
-//     //     sendHitTask: true,
-//     // },
-// })
+const gaId = (process.env.NODE_ENV === 'development') ? gaDevId : gaProductionId
+Vue.use(VueAnalytics, {
+    id: gaId,
+    router,
+})
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', function() {
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const frontendPort = 3005
 const siteName = 'Brian Whicheloe'
+const gaDevId = 'UA-93531536-3'
+const gaProductionId = 'UA-28431964-14'
 
 module.exports = {
     frontendPort,
     siteName,
+    gaDevId,
+    gaProductionId,
 }
