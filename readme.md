bed372d5d0d6ab63dede74aac90df588bf8adac0
Brian Whicheloe <brian@whicheloe.us>
2022-10-17 16:06:02 -0700

    Add readme, specify public dir in config, and fix favicon path

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Whicheloe.us
+My personal website. Built using a pretty much bone stock Svelte and Vite configuration. Statically hosted via render.com.
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // Enable other devices on the network to connect to dev. Useful for testing on real devices.
     server: {
         host: '0.0.0.0',
     },
     plugins: [
         svelte(),
     ],
+    publicDir: 'public',
 })
